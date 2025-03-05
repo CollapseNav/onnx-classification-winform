bfa5bd1d4c34b5950d5b7483eff6e4ba30f6bbbe
CollapseNav <CollapseNav@163.com>
2025-03-05 10:04:25 +0800

    优化使用，可以自行选择cpu和dml

--- Form1.cs
+++ Form1.cs
@@ -4,30 +4,54 @@ namespace onnx_classification_winform;
 
 public partial class Form1 : Form
 {
-    private InferenceSession Inf;
-    private string[] Labels;
+    private InferenceSession? Inf;
+    private const string ModelPath = "onnx_classification_winform.Model.mobilenetv2-10.onnx";
+    private const string LabelPath = "onnx_classification_winform.Model.label_cn.txt";
+    private string[] Labels = ModelLoader.LoadLabels(LabelPath);
     public Form1()
     {
         InitializeComponent();
-        Labels = ModelLoader.LoadLabels("onnx_classification_winform.Model.label_cn.txt");
-        Inf = ModelLoader.LoadModel("onnx_classification_winform.Model.mobilenetv2-10.onnx", Hardware.DML);
-        var inputName = Inf.InputNames[0];
-        var inputMetadata = Inf.InputMetadata[inputName];
-        var dimensions = inputMetadata.Dimensions;
-        dimensions[0] = 1;
+
         Button button = new Button();
         Controls.Add(button);
         button.Text = "选择图片";
+
+        // 添加radiobtn
+        RadioButton useCpu = new RadioButton();
+        RadioButton useDml = new RadioButton();
+        Controls.Add(useCpu);
+        Controls.Add(useDml);
+        useCpu.Text = "使用CPU";
+        useDml.Text = "使用DML";
+        useCpu.Location = new Point(button.Location.X + button.Width + 10, button.Location.Y);
+        useDml.Location = new Point(useCpu.Location.X + useCpu.Width, useCpu.Location.Y);
+        useCpu.Checked = true;
+
         var grid = new DataGridView();
         Controls.Add(grid);
         grid.AutoSize = true;
         grid.Visible = false;
+
         OriginPic form = new OriginPic();
         OriginPic form2 = new OriginPic();
         button.Click += (_, _) =>
         {
+            if (useCpu.Checked)
+                SwitchInf(Hardware.CPU);
+            else if (useDml.Checked)
+                SwitchInf(Hardware.DML);
+
+            var inputName = Inf!.InputNames[0];
+            var inputMetadata = Inf.InputMetadata[inputName];
+            var dimensions = inputMetadata.Dimensions;
+            dimensions[0] = 1;
+
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif";
+            if (form.IsDisposed)
+                form = new OriginPic();
+            if (form2.IsDisposed)
+                form2 = new OriginPic();
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 var bitmap = new Bitmap(openFileDialog.FileName);
@@ -48,7 +72,13 @@ public partial class Form1 : Form
                 grid.Visible = true;
             }
         };
+    }
 
+    private void SwitchInf(Hardware hardware)
+    {
+        if (Inf != null)
+            Inf.Dispose();
+        Inf = ModelLoader.LoadModel(ModelPath, hardware);
     }
 
     private IEnumerable<float> SoftMax(IEnumerable<float> output)
--- OriginPic.cs
+++ OriginPic.cs
@@ -3,13 +3,11 @@ namespace onnx_classification_winform;
 public partial class OriginPic : Form
 {
     private PictureBox Picture = new PictureBox();
-
     public OriginPic()
     {
         InitializeComponent();
         Controls.Add(Picture);
     }
-
     public void LoadImage(Bitmap bitmap)
     {
         Size = new Size(bitmap.Width, 39 + bitmap.Height);
