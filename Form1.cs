78251e2973fb866d3702e5442f29165dfba7de5e
CollapseNav <CollapseNav@163.com>
2025-03-09 23:55:40 +0800

    feat: 简单添加了一个截图的功能

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.ML.OnnxRuntime;
 
 namespace onnx_classification_winform;
@@ -9,77 +10,38 @@ public partial class Form1 : Form
     private const string ModelPath = "onnx_classification_winform.Model.mobilenetv2-10.onnx";
     private const string LabelPath = "onnx_classification_winform.Model.label_cn.txt";
     private string[] Labels = ModelLoader.LoadLabels(LabelPath);
+    private Hardware hw;
+    private ImageCut? ImageCut;
+    private DataGridView Grid;
+    private OriginPic OriginImage = new OriginPic();
+    private OriginPic ResizeImage = new OriginPic();
     public Form1()
     {
         InitializeComponent();
 
-        Button button = new Button();
+        Button showLabels = new Button() { Text = "显示标签" };
+        Controls.Add(showLabels);
+
+        Button button = new Button() { Text = "选择图片", Location = new Point(showLabels.Location.X + showLabels.Width + 10, showLabels.Location.Y) };
         Controls.Add(button);
-        button.Text = "选择图片";
 
-        // 添加radiobtn
-        RadioButton useCpu = new RadioButton();
-        RadioButton useDml = new RadioButton();
-        Controls.Add(useCpu);
-        Controls.Add(useDml);
-        useCpu.Text = "使用CPU";
-        useDml.Text = "使用DML";
-        useCpu.Location = new Point(button.Location.X + button.Width + 10, button.Location.Y);
-        useDml.Location = new Point(useCpu.Location.X + useCpu.Width, useCpu.Location.Y);
-        useCpu.Checked = true;
+        InitImageCut(showLabels);
+        InitRadioButton(button);
+        InitGridView(button);
 
-        var grid = new DataGridView();
-        Controls.Add(grid);
-        grid.AutoSize = true;
-        grid.Visible = false;
-        grid.Location = new Point(button.Location.X, button.Location.Y + button.Height + 10);
-
-        OriginPic form = new OriginPic();
-        OriginPic form2 = new OriginPic();
         button.Click += (_, _) =>
         {
-            grid.Visible = false;
-            if (useCpu.Checked)
-                SwitchInf(Hardware.CPU);
-            else if (useDml.Checked)
-                SwitchInf(Hardware.DML);
-
-            var inputName = Inf!.InputNames[0];
-            var inputMetadata = Inf.InputMetadata[inputName];
-            var dimensions = inputMetadata.Dimensions;
-            dimensions[0] = 1;
-
+            CheckImageForm();
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif";
-            if (form.IsDisposed)
-                form = new OriginPic();
-            if (form2.IsDisposed)
-                form2 = new OriginPic();
+
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 var bitmap = new Bitmap(openFileDialog.FileName);
-                form.LoadImage(bitmap);
-                form.Show();
-                var resizeBitmap = bitmap.Resize(dimensions[2], dimensions[3]);
-                form2.LoadImage(resizeBitmap);
-                form2.Show();
-                var inputs = resizeBitmap.Preprocess(Inf);
-                using var results = Inf.Run(inputs);
-                var outputs = SoftMax(results[0].AsEnumerable<float>());
-                var result = outputs.Select((x, i) => new { x, label = Labels[i] })
-                            .OrderByDescending(x => x.x)
-                            .Take(6)
-                            .ToArray();
-                grid.DataSource = result;
-                grid.Visible = true;
-                grid.Refresh();
+                GetResult(bitmap);
+
             }
         };
-
-        Button showLabels = new Button();
-        Controls.Add(showLabels);
-        showLabels.Text = "显示标签";
-        showLabels.Location = new Point(useDml.Location.X + useDml.Width + 10, useDml.Location.Y);
 
         showLabels.Click += (_, _) =>
         {
@@ -94,13 +56,14 @@ public partial class Form1 : Form
                 row.ItemArray = item.Values.ToArray();
                 dt.Rows.Add(row);
             });
-            grid.DataSource = dt;
-            grid.Visible = true;
-            grid.ScrollBars = ScrollBars.Vertical;
-            grid.Location = new Point(button.Location.X, button.Location.Y + button.Height + 10);
+            Grid.DataSource = dt;
         };
     }
 
+    /// <summary>
+    /// 切换 InferenceSession
+    /// </summary>
+    /// <param name="hardware"></param>
     private void SwitchInf(Hardware hardware)
     {
         if (Inf != null)
@@ -108,9 +71,100 @@ public partial class Form1 : Form
         Inf = ModelLoader.LoadModel(ModelPath, hardware);
     }
 
+    /// <summary>
+    /// 一个简单的Softmax计算
+    /// </summary>
+    /// <param name="output"></param>
+    /// <returns></returns>
     private IEnumerable<float> SoftMax(IEnumerable<float> output)
     {
         float sum = output.Sum(item => (float)Math.Exp(item));
         return output.Select(item => (float)Math.Exp(item) / sum);
+    }
+
+    /// <summary>
+    /// 简单初始化单选按钮radio
+    /// </summary>
+    /// <param name="button"></param>
+    public void InitRadioButton(Button button)
+    {
+        RadioButton useCpu = new RadioButton() { Text = "CPU", Checked = false };
+        RadioButton useDml = new RadioButton() { Text = "DML", Checked = false };
+        Controls.Add(useCpu);
+        Controls.Add(useDml);
+        useCpu.CheckedChanged += (_, _) => SwitchInf(Hardware.CPU);
+        useDml.CheckedChanged += (_, _) => SwitchInf(Hardware.DML);
+        useCpu.Checked = true;
+        useCpu.Location = new Point(button.Location.X + button.Width + 10, button.Location.Y);
+        useDml.Location = new Point(useCpu.Location.X + useCpu.Width, useCpu.Location.Y);
+    }
+
+    /// <summary>
+    /// 简单初始化GridView
+    /// </summary>
+    /// <param name="button"></param>
+    [MemberNotNull("Grid")]
+    public void InitGridView(Button button)
+    {
+        Grid = new DataGridView();
+        Controls.Add(Grid);
+        Grid.AutoSize = true;
+        Grid.Visible = false;
+        Grid.Location = new Point(button.Location.X, button.Location.Y + button.Height + 10);
+        button.Click += (_, _) => Grid.Visible = false;
+        Grid.DataSourceChanged += (_, _) => Grid.Visible = true;
+    }
+
+    public void InitImageCut(Button button)
+    {
+        Button btn = new Button() { Text = "截图" };
+        Controls.Add(btn);
+        btn.Location = new Point(button.Location.X, button.Location.Y + button.Height + 10);
+
+        btn.Click += (_, _) =>
+        {
+            OriginImage.Close();
+            ResizeImage.Close();
+            Hide();
+            Thread.Sleep(300);
+            Bitmap bitmap = new(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(new Point(0, 0), new Point(0, 0), Screen.AllScreens[0].Bounds.Size);
+            //创建新的截图窗口，将刚才的屏幕作为背景
+            ImageCut = new ImageCut { Owner = this, BackgroundImage = bitmap };
+            ImageCut.Cut += GetResult;
+            ImageCut.FormClosed += (_, _) => Show();
+            ImageCut.ShowDialog();
+        };
+    }
+
+    public void GetResult(Bitmap bitmap)
+    {
+        CheckImageForm();
+        var inputName = Inf!.InputNames[0];
+        var inputMetadata = Inf.InputMetadata[inputName];
+        var dimensions = inputMetadata.Dimensions;
+        dimensions[0] = 1;
+        OriginImage.LoadImage(bitmap);
+        OriginImage.Show();
+        var resizeBitmap = bitmap.Resize(dimensions[2], dimensions[3]);
+        ResizeImage.LoadImage(resizeBitmap);
+        ResizeImage.Show();
+        var inputs = resizeBitmap.Preprocess(Inf);
+        using var results = Inf.Run(inputs);
+        var outputs = SoftMax(results[0].AsEnumerable<float>());
+        var result = outputs.Select((x, i) => new { x, label = Labels[i] })
+                    .OrderByDescending(x => x.x)
+                    .Take(6)
+                    .ToArray();
+        Grid.DataSource = result;
+    }
+
+    public void CheckImageForm()
+    {
+        if (OriginImage.IsDisposed)
+            OriginImage = new OriginPic();
+        if (ResizeImage.IsDisposed)
+            ResizeImage = new OriginPic();
     }
 }
